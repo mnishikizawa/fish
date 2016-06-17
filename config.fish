b9bbde90fbdba272723a9c3a929e0ca25fb01763
mnishikizawa <mnishikizawa@tkl-02915.w2k.intra>
2016-06-17 17:18:12 +0900

    modify PATH

--- config.fish
+++ config.fish
@@ -4,8 +4,12 @@ set -gx OMF_PATH "/Users/mnishikizawa/.local/share/omf"
 # Customize Oh My Fish configuration path.
 #set -gx OMF_CONFIG "/Users/mnishikizawa/.config/omf"
 
+set -gx PATH $HOME/.anyenv/bin $PATH
+
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
+
+set fish_theme agnoster
 
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
@@ -13,6 +17,6 @@ function fish_user_key_bindings
   bind \cq 'peco_ghq (commandline -b)'
 end
 
-#function fish_user_key_bindings
-#  bind \cs 'peco_ssh (commandline -b)'
-#end
+function z --description 'jump around'
+  __z $argv
+end
